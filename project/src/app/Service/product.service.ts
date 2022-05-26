f617b55a99d3c841f6aa48bacafdd17624dc9a93
Spirit-Hat <vladonline34@gmail.com>
2022-05-26 21:36:15 +0300

    FIXCSS

--- project/src/app/Service/product.service.ts
+++ project/src/app/Service/product.service.ts
@@ -25,8 +25,8 @@ export class ProductService {
 
   constructor() {
     this.product.push(new product(1,'Test', "Огірок зелений\n" , "./assets/4.png","The main character in the novel is the private detective, Sherlock Holmes. He is presented through the viewpoint of his friend, Dr John Watson, the narrator of the story. Watson describes him as a sharp and highly intelligent individual. ... Holmes takes great pleasure in solving mysteries and outwitting others","200","150"))
-    this.product.push(new product(2,'Test' ,"me" , "./assets/1.jpg","hfuydghfudhu","200","150"))
-    this.product.push(new product(3,'Tests' ,  "to die" , "./assets/1.jpg","fjdklkfdlfklfkdl","200","150"))
+    this.product.push(new product(2,'Test', "Carot\n" , "./assets/carot.png","The main character in the novel is the private detective, Sherlock Holmes. He is presented through the viewpoint of his friend, Dr John Watson, the narrator of the story. Watson describes him as a sharp and highly intelligent individual. ... Holmes takes great pleasure in solving mysteries and outwitting others","200","150"))
+    this.product.push(new product(3,'Test', "Огірок зелений\n" , "./assets/4.png","The main character in the novel is the private detective, Sherlock Holmes. He is presented through the viewpoint of his friend, Dr John Watson, the narrator of the story. Watson describes him as a sharp and highly intelligent individual. ... Holmes takes great pleasure in solving mysteries and outwitting others","200","150"))
 
     // @ts-ignore
     this.fruits.fruits = this.product
